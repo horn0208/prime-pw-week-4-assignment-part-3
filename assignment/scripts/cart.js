ec2a792277a48917988ca467b8f7e896aba3b297
Liz Horn <liz.mpls@gmail.com>
2022-03-15 09:21:20 -0500

    added isFull and removeItem features

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -8,24 +8,27 @@ const maxItems = 5;
 // - take an input parameter for a string `item`
 // - add the new item to the global array `basket`. 
 // - return `true` indicating the item was added
+//   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket. 
+//   - If an item was added to the array, return `true`
+//   - If there was no room and the item could not be added return `false`
 function addItem(item) {
-    console.log('in addItem. Basket before add:', basket);
+    // console.log('in addItem. Basket before add:', basket);
     if (isFull()) {
         return false;
     } else {
         basket.push(item);
-        console.log(`added ${item}. Basket now:`, basket);
+        // console.log(`added ${item}. Basket now:`, basket);
         if (basket[basket.length-1] === item) {
             return true;
         }
     }
 }
-addItem('socks');
-addItem('peaches');
-addItem('oat milk');
-console.log('adding goat milk. should return true:', addItem('goat milk'));
-console.log('adding goat milk again. should return true:', addItem('goat milk'));
-console.log('adding more goat milk, basket should not allow it. should return false:', addItem('goat milk'));
+// addItem('socks');
+// addItem('peaches');
+// addItem('oat milk');
+// console.log('adding goat milk. should return true:', addItem('goat milk'));
+// console.log('adding goat milk again. should return true:', addItem('goat milk'));
+// console.log('adding forbidden ammount of goat milk, basket should not allow. Expect false:', addItem('goat milk'));
 
 // - Create a function called `listItems`. It should:
 // - loop over the items in the `basket` array
@@ -41,7 +44,7 @@ function listItems() {
 // - reset the `basket` to an empty array
 function empty(){
     basket.length = 0;
-    console.log('in empty. basket should be empty:', basket);
+    //console.log('in empty. basket should be empty:', basket);
 }
 
 //  Create a function called isFull(). It should:
@@ -59,16 +62,22 @@ function isFull() {
 // empty();
 // console.log('testing isFull. Basket is empty, should return false:', isFull());
 
-
-// 3. Update the required `addItem` function to:
-//   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket. 
-//   - If an item was added to the array, return `true`
-//   - If there was no room and the item could not be added return `false`
-
-// __Using Array built-in functions!__
-
 // 4. Create a function called `removeItem`. It should:
 //   - Take an input parameter for a string `item`
-//   - Use [Array.indexOf](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf) to find the index of the first matching item in the basket.
-//   - Use [Array.splice](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice) to remove the first matching item from the basket.
+//   - Use [Array.indexOf]to find the index of the first matching item in the basket.
+//   - Use [Array.splice]to remove the first matching item from the basket.
 //   - Return the item removed or `null` if the item was not found
+function removeItem(item) {
+    let removeIndex = basket.indexOf(item);
+    if (removeIndex !== -1) {
+        //console.log(`Index of item to be removed: ${removeIndex}`)
+        let removedItem = basket.splice(removeIndex, 1);
+        return removedItem;
+    } else {
+        return null;
+    }
+}
+// listItems();
+// console.log('removing socks. should return socks', removeItem('socks'));
+// console.log('removing Christopher Walken. should return null', removeItem('Christopher Walken'));
+// listItems();
